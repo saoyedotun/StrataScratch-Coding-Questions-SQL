30168700550792c15356409fd5272c794371d2f6
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-09-01 00:01:53 -0500

    Finding User Purchases, MySQL, PostgreSQL, StrataScratch 10322

--- 10322-finding-user-purchases.sql
+++ 10322-finding-user-purchases.sql
@@ -1,1 +1,8 @@
 -- MySQL, PostgreSQL
+SELECT *
+FROM amazon_transactions a
+JOIN amazon_transactions b
+    ON a.user_id = b.user_id
+    b.created_at >= a.created_at AND
+    b.created_at <= a.created_at + INTERVAL '7 day'
+--     b.created_at <= DATEADD(DAY, 7, a.created_at)
